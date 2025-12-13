57e00057a9457f56601ea3ff3220faea1b9f8377
LuisDS25 <luisfh007@gmail.com>
2025-12-12 22:40:01 -0600

    deploy with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 import github from "@astrojs/github";
 
 export default defineConfig({
-    integrations: [github()],
-    site: "https://luisds25.github.io/portfolio",
+    site: "https://luisds25.github.io",
     base: "/portfolio",
+    integrations: [github()],
 });
