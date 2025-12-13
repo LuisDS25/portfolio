8f1edd17cf266ad0a1b8ab3a6e8f8579d42505d1
LeFDev <156460680+LuisDS25@users.noreply.github.com>
2025-12-12 23:00:49 -0600

    Remove GitHub integration from astro.config.mjs
    Removed GitHub integration from Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
 
 export default defineConfig({
     site: "https://luisds25.github.io",
     base: "/portfolio",
-    integrations: [github()],
 });
