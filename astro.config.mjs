83a828f93a5dd8a4742848401052e152ca1b2f42
LuisDS25 <luisfh007@gmail.com>
2025-12-14 18:07:33 -0600

    backup astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-import github from "@astrojs/github";
+
 
 export default defineConfig({
     site: "https://luisds25.github.io",
     base: "/portfolio",
-    integrations: [github()],
 });
